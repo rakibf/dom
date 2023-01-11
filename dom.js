9be1edaa69174e57800c64262749270cfaf46f7f
Rakib Hassan <mailrakibhassan@gmail.com>
2023-01-11 15:46:52 +0600

    what-is-dom

---
+++ dom.js
@@ -0,0 +1,19 @@
+/* 
+
+- JavaScript makes webpages interactive with the help of DOM
+
+- What is DOM?
+
+1. D for Document => HTML creates the main structure of a website. This HTML file is a document.
+2. O for Object => The HTML document consists of HTML elements. For JavaScript to work with HTML elements, browser converts the elements into JS objects. So, HTML elements are the objects of the document.
+3. M for Model => Every HTML document has a tree structure. This tree structure is called the Model.
+
+- Through DOM we can do CRUD operation.
+
+*/
+
+/* 
+
+
+
+*/
